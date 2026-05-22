2e495d3db05a140e82748182982f0845b0581e07
Ubuntu <ubuntu@ip-172-31-38-213.eu-north-1.compute.internal>
2026-05-22 07:13:19 +0000

    Added Prometheus Flask metrics

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-
 from flask import Flask
+from prometheus_flask_exporter import PrometheusMetrics
 import mysql.connector
 
 app = Flask(__name__)
 
+# Prometheus Metrics
+metrics = PrometheusMetrics(app)
+
 @app.route('/')
 def home():
-    return "GitHub Webhook Working Successfully!..@"
+    return "Jenkins CI/CD Pipeline Working Successfully!"
+
+@app.route('/test')
+def test():
+    return "Flask monitoring working"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
