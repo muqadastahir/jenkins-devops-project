85b70c813cea3082ee6ef82b1345af03a1240068
Ubuntu <ubuntu@ip-172-31-38-213.eu-north-1.compute.internal>
2026-05-22 04:13:03 +0000

    final webhook working

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Automatic Jenkins CI/CD + GitHub Webhook Working Successfully!.."
+    return "Automatic Jenkins CI/CD + GitHub Webhook Working Successfully!....."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
